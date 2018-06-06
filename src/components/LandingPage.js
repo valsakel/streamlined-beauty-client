ddfd30a5202421c018670dd35d80e26f82cd8bfd
Marina Valiquette <marina.valiquette@gmail.com>
2018-06-06 15:47:35 -0400

    Add context and styles for landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import HeaderBar from './HeaderBar';
 
 import './LandingPage.css';
@@ -14,6 +14,71 @@ const LandingPage = (props) => {
     <React.Fragment>
       <div className="container">
         <HeaderBar />
+        <main className="landing-page-main-section">
+          <section className="landing-page-hero-section">
+            <h2 className="landing-page-welcome-header">
+              You came to the place where freelance beauty professionals and consumers connect in metro Atlanta
+            </h2>
+            <div>
+              <Link
+                to={`/signup`}
+                className="landing-page-get-started-link"
+                aria-label="Click to get started"
+              >
+                Get started
+              </Link>
+              <a
+                href="#landing-page-how-works-wrapper"
+                className="landing-page-how-works-link"
+                aria-label="Click to show how it works section"
+              >
+                How it works
+              </a>
+            </div>
+          </section>
+          <section id="landing-page-how-works-wrapper" name="landing-page-how-works-wrapper">
+            <div className="landing-page-how-works-section">
+              <h3>If you're hiring</h3>
+              <div>On SB you will find a list of local freelance
+                beauty professionals that provide services
+                to people within a comfort of their own
+                place.</div>
+              <ol>
+                <li>
+                  Start by signing up with SB
+                </li>
+                <li>
+                  Find a pro that offers the service(s)
+                  you are looking for
+                </li>
+                <li>
+                  Connect with the pro to book an
+                  appointment
+                </li>
+                <li>
+                  Get ready and enjoy
+                </li>
+              </ol>
+            </div>
+            <div className="landing-page-how-works-section">
+              <h3>If you're freelancing</h3>
+              <div>SB is a great place to advertise your
+                services, and to grow your own freelance
+                business</div>
+              <ol>
+                <li>
+                  Start by signing up with SB
+                </li>
+                <li>
+                  Add services to your profile
+                </li>
+                <li>
+                  Get ready for emails from potential clients
+                </li>
+              </ol>
+            </div>
+          </section>
+        </main>
 
       </div>
     </React.Fragment>
